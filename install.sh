37de5375a5af3a5e77d680bcfae9ed7591a3683c
Matsadura <zidanditoo1@gmail.com>
2023-08-29 21:12:00 +0100

    Removed headermaker

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Making a directory for the scripts in home directory
 mkdir -p ~/.scripts
-cp ./bash/gcc ./bash/mk ./bash/push ./python/header_maker.py ./python/files_maker.py ~/.scripts
+cp ./bash/gcc ./bash/mk ./bash/push ./python/files_maker.py ~/.scripts
 
 # Adding vim configuration
 cp -f vimrc ~/.vimrc
