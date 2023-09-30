bd9f299378c5b2ad780d718d5e8eb6f7d5aaa4d2
Matsadura <zidanditoo1@gmail.com>
2023-09-30 22:21:49 +0100

    Fix typos

--- install.sh
+++ install.sh
@@ -117,7 +117,7 @@ ___________.__                   _________
 echo "$art1"
 echo "$art2"
 
-printf "\n\nTime is money, save it with \e[1;32mALX Time Saver\e[0m!\n\n\e[32mcompile\e[0m : Compiles your C files\n\e[32mpush\e[0m : pushes the specified content to your github\n\e[32mmkfiles\e[0m : Extracts all the necessary project files, main.h and all main.c files directly from the intranet\n\e[32mmkscript\e[0m : saves you time setting up a Python or Bash script\n\e[1;33m./read_maker\e[0m : First configure the file then use it to make interactive README directly extracted from your github\n\n"
+printf "\n\nTime is money, save it with \e[1;32mALX Time Saver\e[0m!\n\n\e[32mcompile\e[0m : Compiles your C files\n\e[32mpush\e[0m : pushes the specified content to your github\n\e[32mmkfiles\e[0m : Extracts all the necessary project files, main.h and all main.c files directly from the intranet\n\e[32mmkscript\e[0m : saves you time setting up a Python or Bash script\n\e[1;33m./read_maker\e[0m : First configure the file then use it to make interactive README directly extracted from your github\n\e[32mfixall\e[0m : Fixes the indentation and removes all trailing white spaces in all the files in the current directory or the specified directories\n\e[32msetall\e[0m : Sets up the test suite\n\e[32mtestall\e[0m : Runs the test suite\n\n"
 
 echo -e "\e[31m#DoHardThings\e[0m"
 
